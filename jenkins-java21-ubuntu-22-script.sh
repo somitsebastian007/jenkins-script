b75a054d1c6c56677e6e6d17f247cf9f268f48bc
Somit <74805178+somitsebastian007@users.noreply.github.com>
2025-04-15 19:30:35 +0530

    update

---
+++ jenkins-java21-ubuntu-22-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Updating system packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing OpenJDK 21..."
+sudo apt install openjdk-21-jdk -y
+
+echo "Verifying Java installation..."
+java -version
+
+#Install MAVEN
+sudo apt update
+sudo apt install maven -y
+mvn --version
+
+echo "Adding Jenkins GPG key..."
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo "Adding Jenkins repository..."
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+echo "Updating package list..."
+sudo apt update
+
+echo "Installing Jenkins..."
+sudo apt install jenkins -y
+
+echo "Starting and enabling Jenkins service..."
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+echo "Checking Jenkins status..."
+sudo systemctl status jenkins
+
+echo "Jenkins installation and setup complete."
